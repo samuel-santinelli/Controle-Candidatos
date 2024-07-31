0f4e49c3cb6bce309c08767a9a19eca85e601abf
samuel-santinelli <samuelsantinelli@gmail.com>
2024-07-31 14:23:48 -0300

    feat: initialize project with base structure

---
+++ src/main/java/samuel_santinelli/controle_candidatos/ControleCandidatosApplication.java
@@ -0,0 +1,13 @@
+package samuel_santinelli.controle_candidatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleCandidatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleCandidatosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/samuel_santinelli/controle_candidatos/ControleCandidatosApplicationTests.java
@@ -0,0 +1,13 @@
+package samuel_santinelli.controle_candidatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleCandidatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
